78d271d5be0f3ef7b1350f8dc9646fb1b4153444
kimsungyeul <ksyeul92@gmail.com>
2022-12-30 15:45:05 +0900

    NEON_calculation

---
+++ CUDA_day1/README.md
@@ -0,0 +1,4 @@
+###Day1
+##SIMD(Single Instruction, Multiple Data streams)
+##compile : gcc -o helloneon helloneon.c -O2 -march=armv8-a -mtune=cortex-a72 -mfpu=neon
+##run : ./helloneon
---
+++ CUDA_day1/calculation.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <arm_neon.h>
+
+void printData(uint8x16_t data)
+{
+	printf("%02d ", vgetq_lane_u8(data, 0));
+	printf("%02d ", vgetq_lane_u8(data, 1));
+	printf("%02d ", vgetq_lane_u8(data, 2));
+	printf("%02d ", vgetq_lane_u8(data, 3));
+	printf("%02d ", vgetq_lane_u8(data, 4));
+	printf("%02d ", vgetq_lane_u8(data, 5));
+	printf("%02d ", vgetq_lane_u8(data, 6));
+	printf("%02d ", vgetq_lane_u8(data, 7));
+	printf("%02d ", vgetq_lane_u8(data, 8));
+	printf("%02d ", vgetq_lane_u8(data, 9));
+	printf("%02d ", vgetq_lane_u8(data, 10));
+	printf("%02d ", vgetq_lane_u8(data, 11));
+	printf("%02d ", vgetq_lane_u8(data, 12));
+	printf("%02d ", vgetq_lane_u8(data, 13));
+	printf("%02d ", vgetq_lane_u8(data, 14));
+	printf("%02d ", vgetq_lane_u8(data, 15));
+	printf("\n");
+}
+
+int main(int argc, char **argv)
+{
+	/* 초기화를 위한 데이터셋 */
+	const uint8_t source[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,14 ,15, 16};
+	uint8x16_t data, ret;				/* 계산된 결과를 위한 벡터(데이터) 변수 */
+	uint8x16_t fill3 = vmovq_n_u8(3);	/* 모두 5로채워진 16비트 벡터를 생성 */
+
+	data = vld1q_u8(source);			/* 데이터셋을 벡터에 대입 */
+
+	printf("data : ");
+	printData(data);					/* 벡터에 저장된 값을 출력 */
+
+	ret = vaddq_u8(data, fill3);		/* 두 벡터의 요소들을 각각 더함 */
+	printf("Add: ");
+	printData(ret);						/* 벡터에 저장된 값을 출력*/ 
+
+	ret = vsubq_u8(data, fill3);		/* 두 벡터의 요소들을 각각 더함 */
+	printf("Subtract: ");
+	printData(ret);						/* 벡터에 저장된 값을 출력*/ 
+
+	ret = vmulq_u8(data, fill3);		/* 두 벡터의 요소들을 각각 더함 */
+	printf("Multiply: ");
+	printData(ret);						/* 벡터에 저장된 값을 출력*/ 
+
+	return 0;
+}	
